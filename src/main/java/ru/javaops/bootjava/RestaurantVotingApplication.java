9e1f1e69245bd1866c7128a7f019a5c7e4dee491
christina.gorina <christina.gorina@yandex.ru>
2021-01-02 22:56:35 +0300

    2_03_model_query.patch

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -25,6 +25,7 @@ public class RestaurantVotingApplication implements ApplicationRunner {
 		userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Set.of(Role.ROLE_USER)));
 		userRepository.save(new User("admin@javaops.ru", "Admin_First", "Admin_Last", "admin", Set.of(Role.ROLE_USER, Role.ROLE_ADMIN)));
 		System.out.println(userRepository.findAll());
+		System.out.println(userRepository.findByLastNameContainingIgnoreCase("last"));
 	}
 
 }
